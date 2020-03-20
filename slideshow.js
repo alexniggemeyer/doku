ba81456fd40b21337985d994e976d8e7323c916a
alexniggemeyer <alex.niggemeyer@gmail.com>
2020-03-20 18:35:30 +0100

    renamed:    "IMG/Beh\303\266rde/eingang rathaus.jpg" -> "IMG/Beh\303\266rde/eingang_rathaus.jpg" 	renamed:    "IMG/Beh\303\266rde/nummer in der hand.jpg" -> "IMG/Beh\303\266rde/nummer_in_der_hand.jpg" 	renamed:    "IMG/Beh\303\266rde/warten mit bildschirm.jpg" -> "IMG/Beh\303\266rde/warten_mit_bildschirm.jpg" 	new file:   slideshow.js 	modified:   style.css 	new file:   test.html

---
+++ slideshow.js
@@ -0,0 +1,72 @@
+var pos = 0;
+var ItemCount = 0;
+var itemsWidth = 0;
+
+
+
+$(document).ready(function () {
+
+
+
+
+
+    var dir = "IMG/Behörde/"
+    var ImgLinks = new Array();
+    
+    
+
+
+    $.get( dir, function (data){                        //looking through folder
+        $(data).find('a').each(function(){
+            if(checkImg($(this).attr('href'))){
+               ImgLinks.push($(this).attr('href'));   
+            }
+        });
+
+        $.each(ImgLinks, function(i){                   //adding Path to Img-Tag
+            $('<li class="item"></li>').attr("style", "background-image: url(" + ImgLinks[i] + ");").appendTo('ul');
+        });
+
+        ItemCount = $('.item').length;
+        
+    }); 
+    
+    function checkImg(file){                                            // looking for IMG
+        var extension = file.substr( (file.lastIndexOf('.') +1) );
+        switch(extension) {
+            case 'jpg':
+            case 'png':
+                return true;
+                break;
+            default:
+                return false;
+        }
+
+    };
+});
+
+
+// Moving IMGs
+
+function setTransform() {
+    itemsWidth = $('.items').width();
+    $('.items').attr('style', 'transform: translate(' + (-pos * itemsWidth) + 'px)');
+    console.log(itemsWidth);
+  }
+
+function next(){
+    pos = Math.min(pos + 1, ItemCount - 1);
+    setTransform();
+    console.log(pos);
+
+}
+
+function prev(){
+    pos = Math.max(pos - 1, 0);
+    setTransform();
+    console.log(pos);
+
+}
+
+window.addEventListener('resize', setTransform);
+
